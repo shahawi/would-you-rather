83959c812e7230ad1a3ef5ec36661c392ea19cfc
shahawi <shahawi273@gmail.com>
2021-09-17 06:53:20 +0200

    start with login

---
+++ src/UI/Login.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import LoadingBar from "react-redux-loading";
+
+class Login extends Component {
+  render() {
+    return <div>HI</div>;
+  }
+}
+
+function mapStateToProps({})
+{
+
+
+
+}
+
+export default connect(mapStateToProps)(Login);
