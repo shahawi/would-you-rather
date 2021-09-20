fa5c5a49f6d897c988d773cbe377877c8f4e22aa
shahawi <shahawi273@gmail.com>
2021-09-20 04:36:09 +0200

    Some updates to questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,43 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import '../Home.css'
 
 class Home extends Component {
   componentDidMount() {
-
     if (this.props.authedUser === null) {
       alert("Please login first");
       this.props.history.push(`/Login`);
     }
-
   }
   render() {
-const questions = Object.values(this.props.questions);
+    const questions = Object.values(this.props.questions);
 
     return (
       <Router>
         <Fragment>
-          <div>
+          <div style={{alignSelf:'center', flex: 1, flexWrap: 'wrap'}} >
             {this.props.authedUser !== null && (
-              <ul>
+             <div    style={{
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)'
+          }}>
                 {questions.map((question) => (
-                  
-                  <li key={question.id}>{question.id}</li>
+                  <div className="card" style ={{ width : "25rem" }} key = {question.id}>
+                    <div className="card border-primary mb-3">
+                      <h5 className="card-title">{question.author}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">
+                       Would you rather
+                      </h6>
+                      <p class="card-text" style={{flex: 1, flexWrap: 'wrap'}}> {question.optionOne.text} or ....</p>
+                      <button class="btn btn-primary" type="submit">View poll</button>
+                   
 
-                  )
-                )}
-              </ul>
+
+                    </div>
+                  </div>
+                ))}
+             </div>
             )}
           </div>
         </Fragment>
